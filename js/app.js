83c957019797a232f1f76972cc9060a5f30e7d8b
SamiulxHasanx07 <samiulxhasan650@gmail.com>
2022-03-01 00:50:34 +0600

    design update

--- js/app.js
+++ js/app.js
@@ -11,7 +11,28 @@ fetchData('Data')
 
 
 const displayPhone = allPhone =>{
+
+    const displayPhone = document.getElementById('display-phone');
     allPhone.forEach(singleData =>{
-        console.log(singleData);
+        const singlePhone = document.createElement('div');
+        singlePhone.classList.add('col');
+        // console.log(singleData);
+        singlePhone.innerHTML = `
+        <div class="card">
+            <img class="img-fluid p-3 mx-auto" src="${singleData.image}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title text-center">${singleData.phone_name}</h5>
+                <p class="card-text text-center">Brand: ${singleData.brand}</p>
+                <div class="d-flex justify-content-center">
+                    <button class="btn bg-color text-white">Show Details</button>
+                </div>
+            </div>
+        </div>
+        
+        `;
+        displayPhone.appendChild(singlePhone);
     })
+
+
+
 }
