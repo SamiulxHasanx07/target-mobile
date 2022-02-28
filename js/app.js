a26ae87d1798379557168eb3cd2f003995d0c7a6
SamiulxHasanx07 <samiulxhasan650@gmail.com>
2022-03-01 00:11:57 +0600

    data fetch done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('Connected');
+
+
+const fetchData = phoneName =>{
+    const url = `https://openapi.programming-hero.com/api/phones?`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data => displayPhone(data.data))
+}
+
+fetchData('Data')
+
+
+const displayPhone = allPhone =>{
+    allPhone.forEach(singleData =>{
+        console.log(singleData);
+    })
+}
